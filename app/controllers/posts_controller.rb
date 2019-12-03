49a9909fe101a32dae56c1e5439029940fbd51b8
kazu-taka <k.takahashi@nowall.co.jp>
2019-12-03 14:15:04 +0900

    記事の登録機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:caption)
+    params.require(:post).permit(:caption, :new_image)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+  attribute :new_image
+
+  before_save do
+    self.image = new_image if new_image
+  end
 end
