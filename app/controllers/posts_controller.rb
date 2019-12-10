c32007a9f88c71e0e6f13c5bab6a9cc3d17181ed
kazu-taka <k.takahashi@nowall.co.jp>
2019-12-10 15:06:38 +0900

    リファクタリング完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.order(created_at: :desc)
   end
@@ -17,15 +19,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "投稿を更新しました。"
     else
@@ -34,7 +33,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しました。"
   end
@@ -43,5 +41,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:caption, :new_image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
