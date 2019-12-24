496c0469e6e0281d0e7bd70944f863aa297f511b
kazu-taka <k.takahashi@nowall.co.jp>
2019-12-24 13:43:31 +0900

    commentモデル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :comments, dependent: :destroy
   attribute :new_image
   validates :caption, presence: true
   validate :new_image_check
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   attribute :new_avatar
 
   validates :name, presence: true, length: { maximum: 50 }
