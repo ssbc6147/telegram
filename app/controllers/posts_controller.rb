990ee14337adbaf5aaa3a81a3373cef3227009d5
ssbc6147 <6147mgcn@gmail.com>
2020-01-07 13:56:28 +0900

    N+1問題解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.order(created_at: :desc).page(params[:page]).per(5)
+    @posts = Post.with_attached_image.order(created_at: :desc).page(params[:page]).per(5).includes(user: [avatar_attachment: :blob], comments: [user: [avatar_attachment: :blob]])
     @comment = Comment.new
   end
 
